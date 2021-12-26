291640d19e91c81f56af7eebde30dfbc1991dfa6
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-12-27 05:55:31 +0700

    Integrate with blazegraph and add templates

--- hospitals/views.py
+++ hospitals/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pymantic import sparql
 
+server = sparql.SPARQLServer('http://192.168.100.5:9999/blazegraph/namespace/kb/sparql')
+
+# Loading data to Blazegraph
+server.update('load <https://github.com/dnswd/semanthink/raw/main/hospital-ratings-rdf-5.ttl>')
+
+prefixes = '''
+	PREFIX : <http://example.org/>
+	PREFIX schema: <http://schema.org/>
+	PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+	PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+'''
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    result = server.query(prefixes + 
+		'''
+			SELECT * WHERE { ?s :rating 4 }
+		'''
+	)
+    
+    res = []
+    for b in result['results']['bindings']:
+        res.append(b['s']['value'])
+        
+    context = {
+		'res': res,
+	}
+    
+    return render(request, 'index.html', context)
