5241ce2917de5e3919c80da2a41b0e20b436627f
Dennis Walangadi <dennisaw2000@gmail.com>
2021-12-27 09:42:19 +0700

    fix: legacy code issue

--- install_deps.py
+++ install_deps.py
@@ -1,9 +1,13 @@
 import subprocess
 import sys
 
+
 def install():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "blazegraph-python/dist/pymantic-0.1.1.dev0-py3-none-any.whl"])
+    subprocess.check_call([sys.executable, "-m", "pip",
+                          "install", "-r", "requirements.txt"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install",
+                          "blazegraph-python/dist/pymantic-0.1.1.dev0-py3-none-any.whl"])
+
 
 if __name__ == "__main__":
-  install()
+    install()
