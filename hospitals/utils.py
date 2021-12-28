46498d78a950b64d594d7668a09479203ab04452
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-12-28 12:41:48 +0700

    Update UI

--- hospitals/utils.py
+++ hospitals/utils.py
@@ -15,7 +15,7 @@ prefixes = '''
 def query(q):
 	return server.query(prefixes + q)
 
-def results(res):
+def rs_res(res):
 	return [r['rs']['value'] for r in res['results']['bindings']]
 
 def get_states():
@@ -40,15 +40,15 @@ def get_cities():
 	cities = query(GET_CITIES)
 	return [city['cityName']['value'] for city in cities['results']['bindings']]
 
-def get_types():
+def get_tipes():
 	GET_TYPES = '''
-				SELECT DISTINCT ?type
+				SELECT DISTINCT ?tipe
 				WHERE {
-					?rs :type ?type .
-				} ORDER BY ?type
+					?rs :type ?tipe .
+				} ORDER BY ?tipe
 			'''
-	types = query(GET_TYPES)
-	return [type['type']['value'] for type in types['results']['bindings']]
+	tipes = query(GET_TYPES)
+	return [tipe['tipe']['value'] for tipe in tipes['results']['bindings']]
 
 def get_ownerships():
 	GET_OWNERSHIPS = '''
@@ -70,12 +70,77 @@ def get_ratings():
 	ratings = query(GET_RATINGS)
 	return [rating['rating']['value'] for rating in ratings['results']['bindings']]
 
-def get_emergency_services():
+def get_emergency():
 	GET_EMERGENCIES = '''
 				SELECT DISTINCT ?emergency
 				WHERE {
 					?rs :hasEmergencyServices ?emergency .
 				} ORDER BY DESC(?emergency)
 			'''
-	emergency_services = query(GET_EMERGENCIES)
-	return [emergency['emergency']['value'] for emergency in emergency_services['results']['bindings']]
+	emergency = query(GET_EMERGENCIES)
+	return [emergency['emergency']['value'] for emergency in emergency['results']['bindings']]
+
+def get_rs_by_state(state):
+	GET_STATE = '''
+				SELECT ?rs
+				WHERE {
+					?rs :state ?state .
+					?state foaf:name "%s" .
+				} ORDER BY ?rs
+			''' % state
+	return rs_res(query(GET_STATE))
+
+def get_rs_by_city(city):
+	GET_CITY = '''
+				SELECT ?rs
+				WHERE {
+					?rs :city ?city .
+					?city foaf:name "%s" .
+				} ORDER BY ?rs
+			''' % city
+	return rs_res(query(GET_CITY))
+
+def get_rs_by_tipe(tipe):
+	GET_TYPE = '''
+				SELECT ?rs
+				WHERE {
+					?rs :type "%s" .
+				} ORDER BY ?rs
+			''' % tipe
+	return rs_res(query(GET_TYPE))
+
+def get_rs_by_ownership(ownership):
+	GET_OWNERSHIP = '''
+				SELECT ?rs
+				WHERE {
+					?rs :ownership "%s" .
+				} ORDER BY ?rs
+			''' % ownership
+	return rs_res(query(GET_OWNERSHIP))
+
+def get_rs_by_rating(rating):
+	GET_RATING = '''
+				SELECT ?rs
+				WHERE {
+					?rs :rating "%s" .
+				} ORDER BY ?rs
+			''' % rating
+	return rs_res(query(GET_RATING))
+
+def get_rs_by_emergency(emergency):
+	GET_EMERGENCY = '''
+				SELECT ?rs
+				WHERE {
+					?rs :hasEmergencyServices "%s" .
+				} ORDER BY ?rs
+			''' % emergency
+	return rs_res(query(GET_EMERGENCY))
+
+def get_rs_by_ehr(ehr):
+	GET_EHR = '''
+				SELECT ?rs
+				WHERE {
+					?rs :meetsMeaningfulUseOfEHRs "Y" .
+				} ORDER BY ?rs
+			'''
+	return rs_res(query(GET_EHR))
--- hospitals/views.py
+++ hospitals/views.py
@@ -7,14 +7,14 @@ index_context = dict()
 def index(request):
 	states = get_states()
 	cities = get_cities()
-	types = get_types()
+	tipes = get_tipes()
 	ownerships = get_ownerships()
 	ratings = get_ratings()
-	emergencies = get_emergency_services()
+	emergencies = get_emergency()
 		
 	index_context['states'] = states
 	index_context['cities'] = cities
-	index_context['types'] = types
+	index_context['tipes'] = tipes
 	index_context['ownerships'] = ownerships
 	index_context['ratings'] = ratings
 	index_context['emergencies'] = emergencies
@@ -24,10 +24,28 @@ def index(request):
 def filters(request):  
 	state = request.GET.get('state', '')
 	city = request.GET.get('city', '')
-	tipe = request.GET.get('type', '')
+	tipe = request.GET.get('tipe', '')
 	ownership = request.GET.get('ownership', '')
 	rating = request.GET.get('rating', '')
 	emergency = request.GET.get('emergency', '')
 	ehr = request.GET.get('ehr', '')
- 
+	
+	filters = []
+	if state:
+		filters.extend(get_rs_by_state(state))
+	if city:
+		filters.extend(get_rs_by_city(city))
+	if tipe:
+		filters.extend(get_rs_by_tipe(tipe))
+	if ownership:
+		filters.extend(get_rs_by_ownership(ownership))
+	if rating:
+		filters.extend(get_rs_by_rating(rating))
+	if emergency:
+		filters.extend(get_rs_by_emergency(emergency))
+	if ehr:
+		filters.extend(get_rs_by_ehr(ehr))
+	
+	index_context['filters'] = filters
+  
 	return render(request, 'index.html', index_context)
