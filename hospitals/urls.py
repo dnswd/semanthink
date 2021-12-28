fd1fd3e276c105100bd5c71a2ee5cd76fdd324d1
Dennis Walangadi <dennisaw2000@gmail.com>
2021-12-28 15:18:14 +0700

    feat: fetch hospital info

--- hospitals/urls.py
+++ hospitals/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('?', views.filters, name='filter'),
+    path('kb/', views.kb, name='knowledge_box'),
 ]
--- hospitals/utils.py
+++ hospitals/utils.py
@@ -1,9 +1,12 @@
+from typing import Dict
 from pymantic import sparql
 
-server = sparql.SPARQLServer('http://localhost:9999/blazegraph/namespace/kb/sparql')
+server = sparql.SPARQLServer(
+    'http://localhost:9999/blazegraph/namespace/kb/sparql')
 
 # Loading data to Blazegraph
-server.update('load <https://github.com/dnswd/semanthink/raw/main/hospital-ratings-rdf-5.ttl>')
+server.update(
+    'load <https://github.com/dnswd/semanthink/raw/main/hospital-ratings-rdf-5.ttl>')
 
 prefixes = '''
 	PREFIX : <http://example.org/>
@@ -12,76 +15,85 @@ prefixes = '''
 	PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 '''
 
+
 def query(q):
-	return server.query(prefixes + q)
+    return server.query(prefixes + q)
+
 
 def rs_res(res):
-	return [r['rs']['value'] for r in res['results']['bindings']]
+    return [r['rs']['value'] for r in res['results']['bindings']]
+
 
 def get_states():
-	GET_STATES = '''
+    GET_STATES = '''
 				SELECT DISTINCT ?stateName
 				WHERE {
 					?s :state ?state .
 					?state foaf:name ?stateName .
 				} ORDER BY ?stateName
 			'''
-	states = query(GET_STATES)
-	return [state['stateName']['value'] for state in states['results']['bindings']]
-  
+    states = query(GET_STATES)
+    return [state['stateName']['value'] for state in states['results']['bindings']]
+
+
 def get_cities():
-	GET_CITIES = '''
+    GET_CITIES = '''
 				SELECT DISTINCT ?cityName
 				WHERE {
 					?s :city ?city .
 					?city foaf:name ?cityName .
 				} ORDER BY ?cityName
 			'''
-	cities = query(GET_CITIES)
-	return [city['cityName']['value'] for city in cities['results']['bindings']]
+    cities = query(GET_CITIES)
+    return [city['cityName']['value'] for city in cities['results']['bindings']]
+
 
 def get_tipes():
-	GET_TYPES = '''
+    GET_TYPES = '''
 				SELECT DISTINCT ?tipe
 				WHERE {
 					?rs :type ?tipe .
 				} ORDER BY ?tipe
 			'''
-	tipes = query(GET_TYPES)
-	return [tipe['tipe']['value'] for tipe in tipes['results']['bindings']]
+    tipes = query(GET_TYPES)
+    return [tipe['tipe']['value'] for tipe in tipes['results']['bindings']]
+
 
 def get_ownerships():
-	GET_OWNERSHIPS = '''
+    GET_OWNERSHIPS = '''
 				SELECT DISTINCT ?ownership
 				WHERE {
 					?rs :ownership ?ownership .
 				} ORDER BY ?ownership
 			'''
-	ownerships = query(GET_OWNERSHIPS)
-	return [ownership['ownership']['value'] for ownership in ownerships['results']['bindings']]
+    ownerships = query(GET_OWNERSHIPS)
+    return [ownership['ownership']['value'] for ownership in ownerships['results']['bindings']]
+
 
 def get_ratings():
-	GET_RATINGS = '''
+    GET_RATINGS = '''
 				SELECT DISTINCT ?rating
 				WHERE {
 					?rs :rating ?rating .
 				} ORDER BY DESC(?rating)
 			'''
-	ratings = query(GET_RATINGS)
-	return [rating['rating']['value'] for rating in ratings['results']['bindings']]
+    ratings = query(GET_RATINGS)
+    return [rating['rating']['value'] for rating in ratings['results']['bindings']]
+
 
 def get_emergency():
-	GET_EMERGENCIES = '''
+    GET_EMERGENCIES = '''
 				SELECT DISTINCT ?emergency
 				WHERE {
 					?rs :hasEmergencyServices ?emergency .
 				} ORDER BY DESC(?emergency)
 			'''
-	emergency = query(GET_EMERGENCIES)
-	return [emergency['emergency']['value'] for emergency in emergency['results']['bindings']]
+    emergency = query(GET_EMERGENCIES)
+    return [emergency['emergency']['value'] for emergency in emergency['results']['bindings']]
+
 
 def get_rs(rs):
-	GET_RS = '''
+    GET_RS = '''
 			SELECT ?rs
 			WHERE {
 				?rs a <http://dbpedia.org/resource/Hospital> .
@@ -89,69 +101,118 @@ def get_rs(rs):
 				FILTER (regex(str(?rsName), "%s", "i"))
 			} ORDER BY ?rs
 		''' % rs
-	return rs_res(query(GET_RS))
+    return rs_res(query(GET_RS))
+
 
 def get_rs_by_state(state):
-	GET_STATE = '''
+    GET_STATE = '''
 				SELECT ?rs
 				WHERE {
 					?rs :state ?state .
 					?state foaf:name "%s" .
 				} ORDER BY ?rs
 			''' % state
-	return rs_res(query(GET_STATE))
+    return rs_res(query(GET_STATE))
+
 
 def get_rs_by_city(city):
-	GET_CITY = '''
+    GET_CITY = '''
 				SELECT ?rs
 				WHERE {
 					?rs :city ?city .
 					?city foaf:name "%s" .
 				} ORDER BY ?rs
 			''' % city
-	return rs_res(query(GET_CITY))
+    return rs_res(query(GET_CITY))
+
 
 def get_rs_by_tipe(tipe):
-	GET_TYPE = '''
+    GET_TYPE = '''
 				SELECT ?rs
 				WHERE {
 					?rs :type "%s" .
 				} ORDER BY ?rs
 			''' % tipe
-	return rs_res(query(GET_TYPE))
+    return rs_res(query(GET_TYPE))
+
 
 def get_rs_by_ownership(ownership):
-	GET_OWNERSHIP = '''
+    GET_OWNERSHIP = '''
 				SELECT ?rs
 				WHERE {
 					?rs :ownership "%s" .
 				} ORDER BY ?rs
 			''' % ownership
-	return rs_res(query(GET_OWNERSHIP))
+    return rs_res(query(GET_OWNERSHIP))
+
 
 def get_rs_by_rating(rating):
-	GET_RATING = '''
+    GET_RATING = '''
 				SELECT ?rs
 				WHERE {
 					?rs :rating %s .
 				} ORDER BY ?rs
 			''' % rating
-	return rs_res(query(GET_RATING))
+    return rs_res(query(GET_RATING))
+
 
 def get_rs_by_emergency(emergency):
-	GET_EMERGENCY = '''
+    GET_EMERGENCY = '''
 				SELECT ?rs
 				WHERE {
 					?rs :hasEmergencyServices "%s" .
 				} ORDER BY ?rs
 			''' % emergency
-	return rs_res(query(GET_EMERGENCY))
+    return rs_res(query(GET_EMERGENCY))
+
 
 def get_rs_by_ehr(ehr):
-	GET_EHR = '''
+    GET_EHR = '''
 				SELECT ?rs
 				WHERE {
 					?rs :meetsMeaningfulUseOfEHRs "Y" .
 				} ORDER BY ?rs
 			'''
-	return rs_res(query(GET_EHR))
+    return rs_res(query(GET_EHR))
+
+
+def get_rs_info(rs: str) -> Dict:
+    GET_RS_INFO = (
+        'SELECT * WHERE {'
+        f'''
+			<{rs}> :careEffectiveness ?care_effective ;
+	           :careEffectivenessFootnote ?care_effective_fn ;
+	           :careSafety ?care_safety ;
+	           :careSafetyFootnote ?care_safety_fn ;
+	           :careTimeliness ?care_timeliness ;
+	           :careTimelinessFootnote ?care_timeliness_fn ;
+	           :city ?city_uri ;
+	           :county ?county_uri ;
+	           :hasEmergencyServices ?has_emergency ;
+	           :hasProviderID ?provider_id ;
+	           :medicalImagingEfficientUse ?mi_efficiency ;
+	           :medicalImagingEfficientUseFootnote ?mi_efficiency_fn ;
+	           :meetsMeaningfulUseOfEHRs ?meaningful_ehr ;
+	           :mortality ?mortality ;
+	           :mortalityFootnote ?mortality_fn ;
+	           :ownership ?ownership ;
+	           :patientExperience ?patient_experience ;
+	           :patientExperienceFootnote ?patient_experience_fn ;
+	           :phoneNumber ?phone ;
+	           :rating ?rating ;
+	           :ratingFootnote ?rating_fn ;
+	           :readmission ?readmission ;
+	           :readmissionFootnote ?readmission_dn ;
+	           :state ?state_uri ;
+	           :type ?type ;
+	           :zipCode ?zip_code ;
+	     schema:address ?address ;
+	       foaf:name ?name .
+	
+      ?city_uri foaf:name ?city .
+      ?county_uri foaf:name ?county .
+      ?state_uri foaf:name ?state .
+      '''
+        '}')
+    result = query(GET_RS_INFO)
+    return result['results']['bindings'][0]
