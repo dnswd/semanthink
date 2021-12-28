409fefdd65eee3e74479c3ceda1be6f8b248d0df
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-12-28 13:26:04 +0700

    Add applied filter info

--- hospitals/views.py
+++ hospitals/views.py
@@ -34,22 +34,30 @@ def filters(request):
 	if rs:
 		try:
 			results = get_rs(rs)
+			index_context['q'] = '[Hospital Name] ' + rs
 		except:
 			results = None
 	if state:
 		results = get_rs_by_state(state)
+		index_context['q'] = '[State] ' + state
 	if city:
 		results = get_rs_by_city(city)
+		index_context['q'] = '[City] ' + city
 	if tipe:
 		results = get_rs_by_tipe(tipe)
+		index_context['q'] = '[Type] ' + tipe
 	if ownership:
 		results = get_rs_by_ownership(ownership)
+		index_context['q'] = '[Ownership] ' + ownership
 	if rating:
 		results = get_rs_by_rating(rating)
+		index_context['q'] = '[Rating] ' + rating
 	if emergency:
 		results = get_rs_by_emergency(emergency)
+		index_context['q'] = '[Emergency] ' + emergency
 	if ehr:
 		results = get_rs_by_ehr(ehr)
+		index_context['q'] = '[EHR] ' + 'Yes'
 	
 	index_context['results'] = results
   
