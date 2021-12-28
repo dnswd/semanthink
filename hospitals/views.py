2de885efbd3c8ab1a890d64ab59c8502806c81a2
Gita Permatasari Sujatmiko <gita.permatasari91@ui.ac.id>
2021-12-28 09:29:19 +0700

    Add queries for other filters

--- hospitals/views.py
+++ hospitals/views.py
@@ -19,8 +19,8 @@ def index(request):
 		'''
 			SELECT DISTINCT ?stateName
 			WHERE {
-			?s :state ?state .
-			?state foaf:name ?stateName .
+				?s :state ?state .
+				?state foaf:name ?stateName .
 			} ORDER BY ?stateName
 		'''
 	)
@@ -29,18 +29,62 @@ def index(request):
 		'''
 			SELECT DISTINCT ?cityName
 			WHERE {
-			?s :city ?city .
-			?city foaf:name ?cityName .
+				?s :city ?city .
+				?city foaf:name ?cityName .
 			} ORDER BY ?cityName
+		'''
+	)
+    
+    types = server.query(prefixes + 
+		'''
+			SELECT DISTINCT ?type
+			WHERE {
+				?rs :type ?type .
+			} ORDER BY ?type
+		'''
+	)
+    
+    ownerships = server.query(prefixes + 
+		'''
+			SELECT DISTINCT ?ownership
+			WHERE {
+				?rs :ownership ?ownership .
+			} ORDER BY ?ownership
+		'''
+	)
+    
+    ratings = server.query(prefixes + 
+		'''
+			SELECT DISTINCT ?rating
+			WHERE {
+				?rs :rating ?rating .
+			} ORDER BY DESC(?rating)
+		'''
+	)
+    
+    emergencies = server.query(prefixes + 
+		'''
+			SELECT DISTINCT ?emergency
+			WHERE {
+				?rs :hasEmergencyServices ?emergency .
+			} ORDER BY DESC(?emergency)
 		'''
 	)
     
     states = [state['stateName']['value'] for state in states['results']['bindings']]
     cities = [city['cityName']['value'] for city in cities['results']['bindings']]
+    types = [type['type']['value'] for type in types['results']['bindings']]
+    ownerships = [ownership['ownership']['value'] for ownership in ownerships['results']['bindings']]
+    ratings = [rating['rating']['value'] for rating in ratings['results']['bindings']]
+    emergencies = [emergency['emergency']['value'] for emergency in emergencies['results']['bindings']]
         
     context = {
 		'states': states,
   		'cities': cities,
+		'types': types,
+		'ownerships': ownerships,
+		'ratings': ratings,
+		'emergencies': emergencies,
 	}
     
     return render(request, 'index.html', context)
